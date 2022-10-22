edbc8a108f9cf5c8aeafd99e2e8a31f7e6081b25
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-10-22 23:14:04 +0600

    m

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,11 +1,11 @@
 //our-domain.com/new-meetup
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 function NewMeetupPage() {
-    
+
   function addMeetUpHandler(enteredMeetupData) {
     console.log(enteredMeetupData);
   }
 
-  return <NewMeetupForm onAddMeetup={addMeetUpHandler} />;
+  return  <NewMeetupForm onAddMeetup={addMeetUpHandler} />;
 }
 export default NewMeetupPage;
