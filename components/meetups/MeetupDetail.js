ebb06eedb7a5b6b8edb03215d65b33b616ddc3b6
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-10-23 00:29:25 +0600

    adding

---
+++ components/meetups/MeetupDetail.js
@@ -0,0 +1,15 @@
+import { Fragment } from "react";
+function MeetupDetail() {
+  return (
+    <Fragment>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
+        alt=" A First Meetup "
+      />
+      <h1>A First Meetup</h1>
+      <address> Sector 10, uttara Dhaka</address>
+      <p>The meetup description</p>
+    </Fragment>
+  );
+}
+export default MeetupDetail;
