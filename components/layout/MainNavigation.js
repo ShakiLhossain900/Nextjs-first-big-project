a06ffabd2a4d9fff5e33173f1182f47a641c4fab
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-10-22 23:08:18 +0600

    m

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -9,7 +9,7 @@ function MainNavigation() {
       <nav>
         <ul>
           <li>
-            <Link href='/'>All Meetups</Link>
+            <Link href='/'>All Meetups</Link> 
           </li>
           <li>
             <Link href='/new-meetup'>Add New Meetup</Link>
