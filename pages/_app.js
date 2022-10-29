73c165d66cc3e3346405d6243af732ba5da5992b
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-10-29 18:31:12 +0600

    closing project without completed

--- pages/_app.js
+++ pages/_app.js
@@ -15,3 +15,4 @@ export default MyApp;
 //24 y stop working just watching next and understanding
 //keep working on API route 
 // that all video done wtaching and if after need i will  try to implement if needed
+//mongodb use kore database data sent kore than abar ata fetch kore beckend theke
