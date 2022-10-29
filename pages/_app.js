f955972d875bbd27c2fc9fadcd37cea4d6e3d41f
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-10-29 18:29:59 +0600

    closing project without completed

--- pages/_app.js
+++ pages/_app.js
@@ -14,3 +14,4 @@ export default MyApp;
 
 //24 y stop working just watching next and understanding
 //keep working on API route 
+// that all video done wtaching and if after need i will  try to implement if needed
